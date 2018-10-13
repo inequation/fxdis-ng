12aef6c1d31564fa1cb452c90bf4ea9e4171357f
Leszek Godlewski <github@inequation.org>
2018-10-13 22:49:11 +0200

    Minor project cleanup
    - Remove test.bin from source control (it's recreated by the test)
    - Save test outputs to easily identifiable files for easy diffing
    - Add .gitignore to MSVC project

--- test.bat
+++ test.bat
@@ -1,11 +1,12 @@
 @REM Requires the DirectX SDK bin directory to be in your path!
 
-del test.txt
+del test_fxc.txt
+del test_fxdis.txt
 del test.bin
-fxc /Od /T ps_4_0 test.hlsl /Fo test.bin /Fc test.txt 
+fxc /Od /T ps_4_0 test.hlsl /Fo test.bin /Fc test_fxc.txt 
 @if ERRORLEVEL 1 goto errorexit
 type test.txt
-debug\fxdis.exe test.bin
+debug\fxdis.exe test.bin > test_fxdis.txt
 goto done
 :errorexit
 @ECHO Compile error!
