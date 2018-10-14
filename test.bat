dfdb4d0ccba52f04e29fcc7e1dcf79be7125938b
Leszek Godlewski <github@inequation.org>
2018-10-14 18:37:24 +0200

    Don't output test shader disassembly to console, it only clutters the console

--- test.bat
+++ test.bat
@@ -5,7 +5,6 @@ del test_fxdis.txt
 del test.bin
 fxc /Od /T ps_4_0 test.hlsl /Fo test.bin /Fc test_fxc.txt 
 @if ERRORLEVEL 1 goto errorexit
-type test.txt
 debug\fxdis.exe test.bin > test_fxdis.txt
 goto done
 :errorexit
